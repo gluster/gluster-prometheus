50cf35c3f646dd04cb1fa444a24667876050f38f
Aravinda VK <mail@aravindavk.in>
2018-02-07 20:07:17 +0530

    Infrastructure and a reference metric
    Signed-off-by: Aravinda VK <mail@aravindavk.in>

---
+++ gluster_exporter/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	port = flag.Int("port", 8080, "Exporter Port")
+	metricsPath = flag.String("metrics-path", "/metrics", "Metrics API Path")
+	nodeid = flag.String("nodeid", "", "Gluster Node ID")
+
+	defaultInterval time.Duration = 5
+)
+
+
+type glusterMetric struct {
+	name string
+	fn func()
+	intervalSeconds time.Duration
+}
+
+var glusterMetrics []glusterMetric
+
+func registerMetric(name string, fn func(), intervalSeconds int64) {
+	glusterMetrics = append(glusterMetrics, glusterMetric{name: name, fn: fn, intervalSeconds: time.Duration(intervalSeconds)})
+}
+
+func getNodeID() string {
+	return *nodeid
+}
+
+func main() {
+	flag.Parse()
+
+	// start := time.Now()
+
+	for _, m := range glusterMetrics {
+		go func(m glusterMetric) {
+			for {
+				m.fn()
+				interval := defaultInterval
+				if m.intervalSeconds > 0 {
+					interval = m.intervalSeconds
+				}
+				time.Sleep(time.Duration(time.Second * time.Duration(interval)))
+			}
+		}(m)
+	}
+
+	if len(glusterMetrics) == 0 {
+		fmt.Fprintf(os.Stderr, "No Metrics registered, Exiting..\n")
+		os.Exit(1)
+	}
+	
+	http.Handle(*metricsPath, promhttp.Handler())
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to run exporter\nError: %s", err)
+		os.Exit(1)
+	}
+}
+
---
+++ gluster_exporter/metric_ps.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"strings"
+	"os/exec"
+	"strconv"
+	"io/ioutil"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	glusterProcs = []string{
+		"glusterd",
+		"glusterfsd",
+		"glusterd2",
+		// TODO: Add more processes
+	}
+
+	labels = []string{
+		"volume",
+		"nodeid",
+		"brick_path",
+		"name",
+	}
+
+	glusterCPUPercentage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "cpu_percentage",
+			Help:      "CPU Percentage used by Gluster processes",
+		},
+		labels,
+	)
+
+	glusterMemoryPercentage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "memory_percentage",
+			Help:      "Memory Percentage used by Gluster processes",
+		},
+		labels,
+	)
+
+	glusterResidentMemory = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "resident_memory",
+			Help:      "Resident Memory of Gluster processes",
+		},
+		labels,
+	)
+
+	glusterVirtualMemory = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "virtual_memory",
+			Help:      "Virtual Memory of Gluster processes",
+		},
+		labels,
+	)
+
+	glusterElapsedTime = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "elapsed_time_seconds",
+			Help:      "Elapsed Time of Gluster processes",
+		},
+		labels,
+	)
+
+)
+
+
+func getCmdLine(pid string) []string {
+	var args  []string
+
+	out, err := ioutil.ReadFile("/proc/" + pid + "/cmdline")
+	if err != nil {
+		// TODO: Log Error
+		return args
+	}
+
+	return strings.Split(strings.Trim(string(out), "\x00"), "\x00")
+}
+
+func getGlusterdLabels(cmd string, args []string) prometheus.Labels {
+	return prometheus.Labels{
+		"name": cmd,
+		"volume": "",
+		"nodeid": getNodeID(),
+		"brick_path": "",
+	}
+}
+
+func getGlusterFsdLabels(cmd string, args []string) prometheus.Labels {
+	bpath := ""
+	volume := ""
+
+	prevArg := ""
+	for _, a := range args {
+		if prevArg == "--brick-name" {
+			bpath = a
+		} else if prevArg == "--volfile-id" {
+			volume = strings.Split(a, ".")[0]
+		}
+		prevArg = a
+	}
+
+	return prometheus.Labels{
+		"name": cmd,
+		"volume": volume,
+		"nodeid": getNodeID(),
+		"brick_path": bpath,
+	}
+}
+
+func getUnknownLabels(cmd string, args []string) prometheus.Labels {
+	return prometheus.Labels{
+		"name": cmd,
+		"volume": "",
+		"nodeid": getNodeID(),
+		"brick_path": "",
+	}
+}
+
+func ps() {
+	args := []string{
+		"--no-header",  // No header in the output
+		"-ww",  // To set unlimited width to avoid crop
+		"-o",  // Output Format
+		"pid,pcpu,pmem,rsz,vsz,etimes,comm",
+		"-C",
+		strings.Join(glusterProcs, ","),
+	}
+
+	out, err := exec.Command("ps", args...).Output()
+
+	if err != nil {
+		// TODO: Handle error
+		return
+	}
+
+	for _, line := range strings.Split(string(out), "\n") {
+		// Sample data:
+		// 6959  0.0  0.6 12840 713660  504076 glusterfs
+		lineDataTmp := strings.Split(line, " ")
+		lineData := []string{}
+		for _, d := range lineDataTmp {
+			if strings.Trim(d, " ") == "" {
+				continue
+			}
+			lineData = append(lineData, d)
+		}
+
+		if len(lineData) < 7 {
+			continue
+		}
+		cmdlineArgs := getCmdLine(lineData[0])
+
+		if len(cmdlineArgs) == 0 {
+			// No cmdline file, may be that process died
+			continue
+		}
+
+		var lbls prometheus.Labels
+		switch lineData[6] {
+		case "glusterd": lbls = getGlusterdLabels(lineData[6], cmdlineArgs)
+		case "glusterd2": lbls = getGlusterdLabels(lineData[6], cmdlineArgs)
+		case "glusterfsd": lbls = getGlusterFsdLabels(lineData[6], cmdlineArgs)
+		default: lbls = getUnknownLabels(lineData[6], cmdlineArgs)
+		}
+
+		pcpu, err := strconv.ParseFloat(lineData[1], 64)
+		if err != nil {
+			// TODO: Log Error
+			continue
+		}
+
+		pmem, err := strconv.ParseFloat(lineData[2], 64)
+		if err != nil {
+			// TODO: Log Error
+			continue
+		}
+		rsz, err := strconv.ParseFloat(lineData[3], 64)
+		if err != nil {
+			// TODO: Log Error
+			continue
+		}
+
+		vsz, err := strconv.ParseFloat(lineData[4], 64)
+		if err != nil {
+			// TODO: Log Error
+			continue
+		}
+
+		etimes, err := strconv.ParseFloat(lineData[5], 64)
+		if err != nil {
+			// TODO: Log Error
+			continue
+		}
+
+		// Update the Metrics
+		glusterCPUPercentage.With(lbls).Set(pcpu)
+		glusterMemoryPercentage.With(lbls).Set(pmem)
+		glusterResidentMemory.With(lbls).Set(rsz)
+		glusterVirtualMemory.With(lbls).Set(vsz)
+		glusterElapsedTime.With(lbls).Set(etimes)
+	}
+}
+
+func init() {
+	prometheus.MustRegister(glusterCPUPercentage)
+	prometheus.MustRegister(glusterMemoryPercentage)
+	prometheus.MustRegister(glusterResidentMemory)
+	prometheus.MustRegister(glusterVirtualMemory)
+	prometheus.MustRegister(glusterElapsedTime)
+
+	// Register to update this every 2 seconds
+	// Name, Callback Func, Interval Seconds
+	registerMetric("gluster_ps", ps, 2)
+}
