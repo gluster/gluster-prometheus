409fd78871ca79eccb2bf74ed8d37e13ba2cf8f0
Shubhendu <shtripat@redhat.com>
2018-09-04 11:47:09 +0530

    Added below metrics for volume counters - total no of volumes - started no of volumes - bricks count for volumes
    Signed-off-by: Shubhendu <shtripat@redhat.com>

---
+++ gluster-exporter/metric_volume_counts.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"github.com/gluster/gluster-prometheus/pkg/glusterutils"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	volumeLabels = []string{
+		"volume",
+	}
+
+	glusterVolumeTotalCount = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "volume_total_count",
+			Help:      "Total no of volumes",
+		},
+		[]string{},
+	)
+	glusterVolumeCreatedCount = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "volume_created_count",
+			Help:      "Freshly created no of volumes",
+		},
+		[]string{},
+	)
+	glusterVolumeStartedCount = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "volume_started_count",
+			Help:      "Total no of started volumes",
+		},
+		[]string{},
+	)
+	glusterVolumeBrickCount = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "gluster",
+			Name:      "volume_brick_count",
+			Help:      "Total no of bricks in volume",
+		},
+		volumeLabels,
+	)
+)
+
+func getVolumeLabels(volname string) prometheus.Labels {
+	return prometheus.Labels{
+		"volume": volname,
+	}
+}
+
+func VolumeCounts() {
+	volumes, _ := glusterutils.VolumeInfo(&glusterConfig)
+
+	var volCount, volStartCount, volCreatedCount int
+
+	volCount = len(volumes)
+	for _, volume := range volumes {
+		switch volume.State {
+		case "Started":
+			volStartCount += 1
+		case "Created":
+			volCreatedCount += 1
+		default:
+			// Volume is stopped, nothing to do as the stopped count
+			// could be derived using total - started - created
+		}
+		volBrickCount := 0
+		for _, subvol := range volume.SubVolumes {
+			volBrickCount += len(subvol.Bricks)
+		}
+		glusterVolumeBrickCount.With(getVolumeLabels(volume.Name)).Set(float64(volBrickCount))
+	}
+	glusterVolumeTotalCount.With(prometheus.Labels{}).Set(float64(volCount))
+	glusterVolumeStartedCount.With(prometheus.Labels{}).Set(float64(volStartCount))
+	glusterVolumeCreatedCount.With(prometheus.Labels{}).Set(float64(volCreatedCount))
+}
+
+func init() {
+	prometheus.MustRegister(glusterVolumeTotalCount)
+	prometheus.MustRegister(glusterVolumeStartedCount)
+	prometheus.MustRegister(glusterVolumeCreatedCount)
+	prometheus.MustRegister(glusterVolumeBrickCount)
+
+
+	// Name, Callback Func, Interval Seconds
+	isLeader, err := glusterutils.IsLeader(&glusterConfig)
+	if !isLeader || err != nil {
+		// Unable to find out if the current node is leader
+		// Cannot register volume metrics at this node
+		// TODO: log the error
+		return
+	}
+	registerMetric("gluster_volume_counts", VolumeCounts)
+}
